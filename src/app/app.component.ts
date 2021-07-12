6b20f56fa8b8414604dc13ad364c87b71e2123ca
Septian Adi Wijaya <developadi@gmail.com>
2021-07-12 23:59:49 +0700

    UI - modal setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,4 +32,30 @@ export class AppComponent implements OnInit {
       }
     )
   }
+
+  //ref: https://getbootstrap.com/docs/4.6/components/modal/#modal-components
+  //section: live demo below `Modal Example`
+  public openModalOnMode(employee: Employee, mode: string) {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+
+    button.type = 'button';//default is `submit`
+    //make it invisible
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#employeeModalAdd');
+    } else if (mode === 'edit') {
+      button.setAttribute('data-target', '#employeeModalEdit');
+    } else if (mode === 'delete') {
+      button.setAttribute('data-target', '#employeeModalDelete');
+    }
+
+    //make button appeared on DOM
+    container?.appendChild(button);
+
+    //invoke action click on button
+    button.click();
+  }
 }
